22a4fde116c1eac3707186e98d897890aed19110
PierreAntoineLarge <pierreantoine.large@gmail.com>
2025-05-26 08:20:49 +0200

    add role in bdd

---
+++ migrations/20250526034518-add-role-to-users.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn("Users", "role", {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: "user",
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn("Users", "role");
+  },
+};
