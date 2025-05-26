4720a93c5241d2b9432ba35e88c16c441453cfce
PierreAntoineLarge <pierreantoine.large@gmail.com>
2025-05-26 15:28:25 +0200

    resolving bug

--- src/app.js
+++ src/app.js
@@ -16,13 +16,26 @@ const app = express();
 
 app.use(cors());
 
+const allowedOrigins = [
+  "http://localhost:8081",
+  "https://imn-46q-anonymous-8081.exp.direct",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:8081",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
+
 app.options("*", cors());
 app.use(bodyParser.json());
 
