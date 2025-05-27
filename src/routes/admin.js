015a81a66b0638252bcc36ca0f09aa191eede6ac
PierreAntoineLarge <pierreantoine.large@gmail.com>
2025-05-27 12:04:39 +0200

    route get users

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,6 +4,7 @@ const {
   authorizeRole,
   authenticateJWT,
 } = require("../middleware/authorizeRole");
+const { User } = require("../../models");
 
 router.post(
   "/admin/dashboard",
@@ -12,6 +13,22 @@ router.post(
   (req, res) => {
     console.log("titi");
     res.json({ message: "Welcome admin!" });
+  }
+);
+
+router.get(
+  "/admin/users",
+  authenticateJWT,
+  authorizeRole("admin"),
+  async (req, res) => {
+    try {
+      const users = await User.findAll();
+      console.log(users);
+      res.json({ message: "Users found!", users });
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
   }
 );
 
