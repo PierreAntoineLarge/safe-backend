93e3e5b832af406672d25ad8f6a159cf77c95122
PierreAntoineLarge <pierreantoine.large@gmail.com>
2025-09-18 19:00:26 +0200

    eslin update

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from "eslint-define-config";
 
 export default defineConfig({
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
+  rules: {
+    semi: ["error", "always"],
+    "no-unused-vars": "warn",
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  rules: {},
 });
