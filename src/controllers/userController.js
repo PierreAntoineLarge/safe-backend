0a82306b96544668c25d1d7029363ae58e307965
PierreAntoineLarge <pierreantoine.large@gmail.com>
2025-09-18 16:04:27 +0200

    test sonarcube

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,30 @@
+const { body, validationResult } = require("express-validator");
 const User = require("../../models/user");
 
 async function updateEmergencyContact(req, res) {
   try {
+    await Promise.all([
+      body("emergencyContactName")
+        .isString()
+        .withMessage("Le nom doit être une chaîne de caractères")
+        .notEmpty()
+        .withMessage("Le nom du contact d'urgence est obligatoire")
+        .run(req),
+
+      body("emergencyContactEmail")
+        .isEmail()
+        .withMessage("L'email doit être valide")
+        .normalizeEmail()
+        .run(req),
+    ]);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const { emergencyContactName, emergencyContactEmail } = req.body;
     const userId = req.user.id;
-
-    if (!emergencyContactName || !emergencyContactEmail) {
-      return res.status(400).json({ error: "Champs manquants" });
-    }
 
     const user = await User.findByIdAndUpdate(
       userId,
@@ -15,11 +32,14 @@ async function updateEmergencyContact(req, res) {
       { new: true }
     );
 
+    if (!user) {
+      return res.status(404).json({ error: "Utilisateur non trouvé" });
+    }
+
     res.json({ message: "Contact d'urgence mis à jour", user });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Erreur serveur" });
   }
 }
-
 module.exports = { updateEmergencyContact };
