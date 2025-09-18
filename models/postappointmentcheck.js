d393d09eea699b61057e8ba6a5e7c6b5bede9b00
PierreAntoineLarge <pierreantoine.large@gmail.com>
2025-09-18 19:07:51 +0200

    eslin update

--- models/postappointmentcheck.js
+++ models/postappointmentcheck.js
@@ -1,28 +1,20 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class PostAppointmentCheck extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+  class PostAppointmentCheck extends Model {}
+  PostAppointmentCheck.init(
+    {
+      appointmentId: DataTypes.INTEGER,
+      notification_sent_at: DataTypes.DATE,
+      response_received_at: DataTypes.DATE,
+      status: DataTypes.STRING,
+      support_option_selected: DataTypes.STRING,
+      alert_sent_to_contact: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "PostAppointmentCheck",
     }
-  }
-  PostAppointmentCheck.init({
-    appointmentId: DataTypes.INTEGER,
-    notification_sent_at: DataTypes.DATE,
-    response_received_at: DataTypes.DATE,
-    status: DataTypes.STRING,
-    support_option_selected: DataTypes.STRING,
-    alert_sent_to_contact: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'PostAppointmentCheck',
-  });
+  );
   return PostAppointmentCheck;
 };
